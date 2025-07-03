a5bb916e18c4fba21ad0e15e98040ecd663579ea
milanfusco <milanfusco68@gmail.com>
2025-07-03 13:49:37 -0400

    Refactor (core): moved Sidebar.tsx from @/components/ to @/components/globalComponents/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/globals.css'
-import Sidebar from '@/components/Sidebar'
+import Sidebar from '@/components/globalComponents/Sidebar'
 import React from 'react'
 
 export const metadata = {
